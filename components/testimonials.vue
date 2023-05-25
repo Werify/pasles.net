641b878af01413a4bb15865d3c1d084d8f7a358a
navidjaberi <104099945+navidjaberi@users.noreply.github.com>
2023-05-25 10:08:37 -0700

    setup testimonials transition

--- components/testimonials.vue
+++ components/testimonials.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div class="py-8  mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-md text-center">
         <h2
           class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
@@ -14,383 +14,111 @@
           elements built with the utility classes from Tailwind
         </p>
       </div>
-      <div class="grid gap-8 lg:grid-cols-3 overflow-hidden relative -mx-4  mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3">
+      <div class="grid gap-8 lg:grid-cols-3 overflow-hidden relative xl:-mx-6   mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3">
         <div class="space-y-6 py-4 animate-first">
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800" style="animation-delay: 0.4s;" >
+          <figure
+           v-for="(item) in testimonialsFirstRow" 
+           :key="item"
+            class="p-6 bg-gray-50 rounded dark:bg-gray-800" :style="item.animationDelay" >
             <blockquote class="text-sm text-gray-500 dark:text-gray-400">
               <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Solid foundation for any project
+               {{item.message.title}}
               </h3>
               <p class="my-4">
-                "This is a very complex and beautiful set of elements. Under the
-                hood it comes with the best things from 2 different worlds:
-                Figma and Tailwind.”
+               {{item.message.des}}
               </p>
             </blockquote>
             <figcaption class="flex items-center space-x-3">
               <img
                 class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                :src="item.user.profilePic"
                 alt="profile picture"
               />
               <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Bonnie Green</div>
+                <div>{{item.user.name}}</div>
                 <div
                   class="text-sm font-light text-gray-500 dark:text-gray-400"
                 >
-                  CTO at Open AI
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800" style="animation-delay: 0.4s;" >
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Solid foundation for any project
-              </h3>
-              <p class="my-4">
-                "This is a very complex and beautiful set of elements. Under the
-                hood it comes with the best things from 2 different worlds:
-                Figma and Tailwind.”
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Bonnie Green</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  CTO at Open AI
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800" style="animation-delay: 0.5s;">
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                A must-have for designers
-              </h3>
-              <p class="my-4">
-                "This is a very complex and beautiful set of elements. Under the
-                hood it comes with the best things from 2 different worlds:
-                Figma and Tailwind.”
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Lana Byrd</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  Software Engineer at Tesla
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800" >
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Speechless with how easy this was to integrate
-              </h3>
-              <p class="my-4">
-                "I recently got my hands on Flowbite Pro, and holy crap, I'm
-                speechless with how easy this was to integrate within my
-                application. Most templates are a pain, code is scattered, and
-                near impossible to theme."
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Jese Leos</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  CEO at Oracle
+                  {{item.user.job}}
                 </div>
               </div>
             </figcaption>
           </figure>
         </div>
         <div class="space-y-6 py-4 animate-second">
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
+            <figure
+           v-for="(item) in testimonialsSecondRow" 
+           :key="item"
+            class="p-6 bg-gray-50 rounded dark:bg-gray-800" :style="item.animationDelay" >
             <blockquote class="text-sm text-gray-500 dark:text-gray-400">
               <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Efficient Collaborating
+               {{item.message.title}}
               </h3>
               <p class="my-4">
-                "Flowbite is just awesome. It contains tons of predesigned
-                components and pages starting from login screen to complex
-                dashboard. Perfect choice for your next SaaS application.”
+               {{item.message.des}}
               </p>
             </blockquote>
             <figcaption class="flex items-center space-x-3">
               <img
                 class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png"
+                :src="item.user.profilePic"
                 alt="profile picture"
               />
               <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Joseph McFall</div>
+                <div>{{item.user.name}}</div>
                 <div
                   class="text-sm font-light text-gray-500 dark:text-gray-400"
                 >
-                  Junior Designer at Adobe
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Efficient Collaborating
-              </h3>
-              <p class="my-4">
-                "Flowbite is just awesome. It contains tons of predesigned
-                components and pages starting from login screen to complex
-                dashboard. Perfect choice for your next SaaS application.”
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Joseph McFall</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  Junior Designer at Adobe
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Mindblowing workflow and variants
-              </h3>
-              <p class="my-4">
-                "Flowbite provides a robust set of design tokens and components
-                based on the popular Tailwind CSS framework.
-              </p>
-              <p class="my-4">
-                From the most used UI components like forms and navigation bars
-                to the whole app screens designed both for desktop and mobile,
-                this UI kit provides a solid foundation for any project.”
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/helene-engels.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Helene Engels</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  CFO at Microsoft
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Perfect choice for a SaaS application
-              </h3>
-              <p class="my-4">
-                "Flowbite provides a robust set of design tokens and components
-                based on the popular Tailwind CSS framework.
-              </p>
-              <p class="my-4">
-                From the most used UI components like forms and navigation bars
-                to the whole app screens designed both for desktop and mobile,
-                this UI kit provides a solid foundation for any project.”
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Leslie Livingston</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  Creative Director at Apple
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Perfect choice for a SaaS application
-              </h3>
-              <p class="my-4">
-                "Flowbite provides a robust set of design tokens and components
-                based on the popular Tailwind CSS framework.
-              </p>
-              <p class="my-4">
-                From the most used UI components like forms and navigation bars
-                to the whole app screens designed both for desktop and mobile,
-                this UI kit provides a solid foundation for any project.”
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Leslie Livingston</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  Creative Director at Apple
+                  {{item.user.job}}
                 </div>
               </div>
             </figcaption>
           </figure>
         </div>
         <div class="space-y-6 py-4 animate-third">
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
+          <figure
+           v-for="(item) in testimonialsThirdRow" 
+           :key="item"
+            class="p-6 bg-gray-50 rounded dark:bg-gray-800" :style="item.animationDelay" >
             <blockquote class="text-sm text-gray-500 dark:text-gray-400">
               <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Solid foundation for any project
+               {{item.message.title}}
               </h3>
               <p class="my-4">
-                "I recently got my hands on Flowbite Pro, and holy crap, I'm
-                speechless with how easy this was to integrate within my
-                application. Most templates are a pain, code is scattered, and
-                near impossible to theme."
+               {{item.message.des}}
               </p>
             </blockquote>
             <figcaption class="flex items-center space-x-3">
               <img
                 class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
+                :src="item.user.profilePic"
                 alt="profile picture"
               />
               <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Michael Gough</div>
+                <div>{{item.user.name}}</div>
                 <div
                   class="text-sm font-light text-gray-500 dark:text-gray-400"
                 >
-                  Front-end engineer at Meta
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Solid foundation for any project
-              </h3>
-              <p class="my-4">
-                "I recently got my hands on Flowbite Pro, and holy crap, I'm
-                speechless with how easy this was to integrate within my
-                application. Most templates are a pain, code is scattered, and
-                near impossible to theme."
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Michael Gough</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  Front-end engineer at Meta
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Mindblowing workflow and variants
-              </h3>
-              <p class="my-4">
-                "Flowbite is just awesome. It contains tons of predesigned
-                components and pages starting from login screen to complex
-                dashboard. Perfect choice for your next SaaS application.”
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/neil-sims.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Neil Sims</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  Software architect at Amazon
-                </div>
-              </div>
-            </figcaption>
-          </figure>
-          <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-            <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Speechless with how easy this was to integrate
-              </h3>
-              <p class="my-4">
-                "This is a very complex and beautiful set of elements. Under the
-                hood it comes with the best things from 2 different worlds:
-                Figma and Tailwind.”
-              </p>
-            </blockquote>
-            <figcaption class="flex items-center space-x-3">
-              <img
-                class="w-9 h-9 rounded-full"
-                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/robert-brown.png"
-                alt="profile picture"
-              />
-              <div class="space-y-0.5 font-medium dark:text-white">
-                <div>Robert Brown</div>
-                <div
-                  class="text-sm font-light text-gray-500 dark:text-gray-400"
-                >
-                  Junior developer at SAP
+                  {{item.user.job}}
                 </div>
               </div>
             </figcaption>
           </figure>
         </div>
+        <div class="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-white dark:from-gray-900 "></div>
+        <div class="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-white dark:from-gray-900 "></div>
       </div>
     </div>
   </section>
 </template>
+<script setup lang="ts">
+import { firstRow,secondRow,thirdRow} from "../data/testimonials/testimonials";
+
+const testimonialsFirstRow=ref(firstRow)
+const testimonialsSecondRow=ref(secondRow)
+const testimonialsThirdRow=ref(thirdRow)
+
+</script>
 <style scoped>
 .animate-first{
   --marquee-duration: 21000ms;
